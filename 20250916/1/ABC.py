6e1430419a032d1932d62c7a0b7cc4b03c7468b8
Renkas Vladislav <Renkasvv@my.msu.ru>
2025-09-16 16:36:45 +0300

    ABC classes

---
+++ 20250916/1/ABC.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if (num % 2 == 0) and (num % 25 == 0):
+    print("A +", end = " ")
+else:
+    print("A -", end = " ")
+
+if (num % 2 == 1) and (num % 25 == 0):
+    print("B +", end = " ")
+else:
+    print("B -", end = " ")
+
+if num % 8 == 0:
+    print("C +")
+else:
+    print("C -")  
