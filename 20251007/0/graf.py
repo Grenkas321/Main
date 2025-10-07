3c4dbf21f75d36f79b669e8170b1ffb40f0dddc4
Renkas Vladislav <Renkasvv@my.msu.ru>
2025-10-07 11:34:06 +0300

    sinM

--- 20251007/0/graf.py
+++ 20251007/0/graf.py
@@ -2,6 +2,8 @@
 from math import sin
 W,H = eval(input())
 A,B = eval(input())
+screen = [['.'] *W for i in range(H)]
+sc = "\n".join("".join(line) for line in screen)
 
 def scale(a,b, A, B, x):
     return (x - a)*(B-A)/(b-a) + A
@@ -10,4 +12,4 @@ for line in range(0, H):
     x = scale(0, H-1, A, B, line)
     y = sin(x)
     k = round(scale(-1,1,0,W-1,y)) 
-    print(" "*k, "*")
+    sc[line][k+1] = "#"
