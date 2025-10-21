e09d2d6291bef236fba3ccf1ef56d0e9c0e618f5
Renkas Vladislav <Renkasvv@my.msu.ru>
2025-10-21 10:47:13 +0300

    First yield

--- 20251021/0/yield.py
+++ 20251021/0/yield.py
@@ -1,12 +1,13 @@
 from itertools import count
 
-def gen():
+def gen(n):
     s = 0
-    for i in count(1):
+    for i in count(n+1):
         s += 1/(i*i)
         yield s
 
 g = sum(1/(i*i) for i in range(1, 10000))
 
-print(gen)
+n = int(input())
+print(gen(n))
 print(g)
